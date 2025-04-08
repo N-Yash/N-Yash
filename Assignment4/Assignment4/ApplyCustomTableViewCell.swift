82fc86fe9983fcb11ebce691dc0edb0ec5be431e
N-Yash <113545575+N-Yash@users.noreply.github.com>
2025-04-07 22:23:19 -0400

    commit

---
+++ Assignment4/Assignment4/ApplyCustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ApplyCustomTableViewCell.swift
+//  Assignment4
+//
+//  Created by Yash Vipul Naik on 2025-04-07.
+//
+
+import UIKit
+
+class ApplyCustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var applyLinkLable: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- Assignment4/Assignment4/JobDetailsViewController.swift
+++ Assignment4/Assignment4/JobDetailsViewController.swift
@@ -11,11 +11,39 @@ class JobDetailsViewController: UIViewController {
 
     var selectedJobReceived : JobsArrayModel? = nil
     
+    @IBOutlet weak var jobTitleLabel: UILabel!
+    @IBOutlet weak var jobCountryLabel: UILabel!
+    @IBOutlet weak var jobSalaryLabel: UILabel!
+    @IBOutlet weak var jobTypeLabel: UILabel!
+    @IBOutlet weak var jobDescriptionTextView: UITextView!
+    @IBOutlet weak var applyTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        jobTitleLabel.text = selectedJobReceived?.title
+        jobCountryLabel.text = selectedJobReceived?.company
+        jobSalaryLabel.text = "Salary: " + (selectedJobReceived?.salaryRange ?? "")
+        jobTypeLabel.text = "Job Type: " + (selectedJobReceived?.employmentType ?? "")
+        jobDescriptionTextView.text = selectedJobReceived?.description
     }
 
-
+    func numberOfSections(in applyTableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    func tableView(_ applyTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return selectedJobReceived?.jobProviders.count ?? 0
+    }
+    
+    func tableView(_ applyTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = applyTableView.dequeueReusableCell(withIdentifier: "apply", for: indexPath) as? ApplyCustomTableViewCell
+        
+        cell?.applyLinkLable.text = selectedJobReceived?.jobProviders[indexPath.row].jobProvider
+        return cell!
+    }
     /*
     // MARK: - Navigation
 
