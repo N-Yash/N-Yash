16b5cfd872f700f6c81eee1a20b0c44f268e7acc
N-Yash <113545575+N-Yash@users.noreply.github.com>
2025-04-11 18:36:11 -0400

    commit

--- Assignment4/Assignment4/JobDetailsViewController.swift
+++ Assignment4/Assignment4/JobDetailsViewController.swift
@@ -7,51 +7,52 @@
 
 import UIKit
 
-class JobDetailsViewController: UIViewController {
+class JobDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     var selectedJobReceived : JobsArrayModel? = nil
-    
+
     @IBOutlet weak var jobTitleLabel: UILabel!
     @IBOutlet weak var jobCountryLabel: UILabel!
     @IBOutlet weak var jobSalaryLabel: UILabel!
     @IBOutlet weak var jobTypeLabel: UILabel!
     @IBOutlet weak var jobDescriptionTextView: UITextView!
     @IBOutlet weak var applyTableView: UITableView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print(selectedJobReceived?.title ?? "")
         jobTitleLabel.text = selectedJobReceived?.title
         jobCountryLabel.text = selectedJobReceived?.company
         jobSalaryLabel.text = "Salary: " + (selectedJobReceived?.salaryRange ?? "")
         jobTypeLabel.text = "Job Type: " + (selectedJobReceived?.employmentType ?? "")
         jobDescriptionTextView.text = selectedJobReceived?.description
+        applyTableView.dataSource = self // Don't forget to set the dataSource
+        applyTableView.reloadData()
+        applyTableView.delegate = self
     }
 
-    func numberOfSections(in applyTableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    func numberOfSections(in tableView: UITableView) -> Int { // Use the tableView parameter
         return 1
     }
-    
-    func tableView(_ applyTableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int { // Use the tableView parameter
         return selectedJobReceived?.jobProviders.count ?? 0
     }
-    
-    func tableView(_ applyTableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = applyTableView.dequeueReusableCell(withIdentifier: "apply", for: indexPath) as? ApplyCustomTableViewCell
-        
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell { // Use the tableView parameter
+        let cell = tableView.dequeueReusableCell(withIdentifier: "apply", for: indexPath) as? ApplyCustomTableViewCell
+
         cell?.applyLinkLable.text = selectedJobReceived?.jobProviders[indexPath.row].jobProvider
         return cell!
     }
-    /*
-    // MARK: - Navigation
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let jobProvider = selectedJobReceived?.jobProviders[indexPath.row],
+        let url = URL(string: jobProvider.url) {
+            UIApplication.shared.open(url) // Open the URL in the browser
+        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        // Optionally deselect the row for visual feedback
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
-
 }
